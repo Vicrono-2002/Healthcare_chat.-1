150c234bce203aab9a199a50f3fc86e8cee65472
victor <vicron@localhost.localdomain>
2025-07-19 14:02:45 -0400

    folder structure one

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="HEALTHCARE-SYMPTOM-ANALYSIS-CHATBOT",
+    version="0.0.1",
+    author="Kiprotich Victor, Gideon Koech",
+    author_email="respectrono@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
